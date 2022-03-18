7d1e0ca4a86e14bdced07a83e9d8fd38f8e7c85d
kohya suzuki <44775152+kohya-suzuki@users.noreply.github.com>
2022-03-18 11:09:29 +0900

    .DS_Storeファイルのgitignoreへの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :username])
+    # devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+        # devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 
   def after_sign_in_path_for(resource)
     if current_user
@@ -21,10 +25,6 @@ class ApplicationController < ActionController::Base
     home_index_path
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
-  end
 
   
 
